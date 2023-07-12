b0249abce8cf203e33c0b0ffb44abb13dfde9f36
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-07-12 14:46:33 +0300

    create AuthProvider component for login

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 export default function Navbar() {
+    const session = {}
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -16,6 +18,19 @@ export default function Navbar() {
             </Link>
           ))}
         </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+            <>
+            UserPhoto
+
+            <Link href="/create-project">
+                Share Work
+            </Link>
+            </>
+        ) : (
+            <AuthProviders/>
+        )}
       </div>
     </nav>
   );
