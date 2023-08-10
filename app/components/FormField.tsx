54f7c622ab288558439ac04d4479add4f85d7fbe
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-08-11 02:31:16 +0300

    create the Formfield component for reusing

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,16 +1,44 @@
 type Props = {
-    type?: string; 
-    title: string;
-    state: string;
-    placeholder: string;
-    isTextArea?: boolean;
-    setState: (value: string) => void;
-}
+  type?: string;
+  title: string;
+  state: string;
+  placeholder: string;
+  isTextArea?: boolean;
+  setState: (value: string) => void;
+};
 
-const FormField = ({type, title, state, placeholder, isTextArea, setState}: Props) => {
+const FormField = ({
+  type,
+  title,
+  state,
+  placeholder,
+  isTextArea,
+  setState,
+}: Props) => {
   return (
-    <div>FormField</div>
-  )
-}
+    <div className="flexStart flex-col w-full gap-4">
+      <label className="w-full text-gray-100">{title}</label>
 
-export default FormField
+      {isTextArea ? (
+        <textarea
+          className="form_field-input"
+          placeholder={placeholder}
+          value={state}
+          required
+          onChange={(e) => setState(e.target.value)}
+        />
+      ) : (
+        <input
+          type={type || "text"}
+          className="form_field-input"
+          placeholder={placeholder}
+          value={state}
+          required
+          onChange={(e) => setState(e.target.value)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default FormField;
