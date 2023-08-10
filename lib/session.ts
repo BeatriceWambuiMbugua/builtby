4b3ad8d86bffc808ae478ab2eb4073b5d137bf7c
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-08-10 14:22:05 +0300

    add the token and grafbase

--- lib/session.ts
+++ lib/session.ts
@@ -1,58 +1,88 @@
 import { NextAuthOptions, User } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 import { getServerSession } from "next-auth/next";
 import { AdapterUser } from "next-auth/adapters";
-import jsonwebtoken from "jsonwebtoken"
+import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.types";
+import { SessionInterface, UserProfile } from "@/common.types";
+import { createUser, getUser } from "./action";
 export const authOptions: NextAuthOptions = {
-providers: [
+  providers: [
     GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY!,
+    }),
+  ],
+  jwt: {
+  encode: ({secret, token}) =>{
+    const encodedToken = jsonwebtoken.sign({
+        ...token, 
+        iss: 'grafbase', 
+        exp: Math.floor(Date.now()/1000) + 60 * 60 }, secret)
+        return encodedToken
+    
+  },
+  decode: async ({secret, token}) =>{
+    const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
 
-        clientId: process.env.GOOGLE_CLIENT_ID!, 
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY!,
-    })
-], 
-// jwt: {
-// encode: ({secret, token}) =>{},
-// decode: async ({secret, token}) =>{}
-// }, 
+    return decodedToken;
+  }
+  },
 
-theme: {
-    colorScheme: 'light', 
-    logo: '/logo.png'
-}, 
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.png",
+  },
 
-callbacks: {
-    async session ({session}){
-        return session;
-
-    }, 
-    async signIn({user}: {user: AdapterUser | User}){
+  callbacks: {
+    async session({ session }) {
+        const email = session?.user?.email as string;
         try {
-            //get the user if they exist
+            const data = await getUser(email) as {user?: UserProfile}
 
-
-            //if they do not exist, create them
-
-
-            //return true
-
-            return true
-
-            
-        } catch (error: any){
-            console.log(error)
-            return false
-
-            
+            const newSession = {
+                ...session,
+                user:{
+                    ...session.user,
+                    ...data?.user
+                }
+            }
+            return newSession;
+        } catch (error) {
+            console.log('Error retrieving user data', error)
+            return session
         }
-    }
-}
-}
+    
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        //get the user if they exist
+        const userExists = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
 
-export async function getCurrentUser(){
-    const session = await getServerSession(authOptions) as SessionInterface;
+        //if they do not exist, create them
+        if (!userExists.user) {
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          );
+        }
 
-    return session;
+        //return true
+
+        return true;
+      } catch (error: any) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
 }
